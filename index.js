3531fd5d262fb91d368c7a8e7dcfe3e4a7a99046
bakar mokhtar <walaalpakar08@gmail.com>
2022-12-11 07:45:19 +0300

    waan badalay

--- index.js
+++ index.js
@@ -1,121 +1,46 @@
-/*🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️ Task 1: Warm-up!🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️🏋️‍♂️*/
+/* Ansewrs */
 
-/*
-Task 1 - Voting Age
-
-Do the following:   
-   1. Make a variable called votingAge and give it a value
-   2. Return true if age is 18 or higher
-
-*/
+let age =15;
+if(age>=18){
+  console.log("Gaari Waa waa wadi kartaa")
+}else{
+  console.log("Gaari mawadi kartid")
+}
 
 
-
-/*
-Task 2 - Values
-
-Do the following:   
-   1. Declare two variables and assign them values
-   2. Use a conditional to change the value of the 1st variable based on the value assigned to the 2nd variable
-   3. Return the new value of the 1st variable
-
-*/
+let num1 =5;
+let num2 =8;
+let sum=num1+2;
+console.log(sum)
 
 
+const num=1999
+console.log(typeof(num))
 
-/*
-Task 3 - Convert Strings to Numbers
+let num =10;
+if(num%2){
+	console.log("odd")
+}else{
+	console.log("even")
+}
 
-Do the following:   
-   1. Declare a string type variable with the value of "1999"
-   2. Convert the string value of "1999" to a integer value of 1999
-   3. Return the result
+let i=0;
+for(i=0; i<=100; i++){
+	console.log(i)
+}
+let num=21%5;
+console.log(num)
 
-   HINT: look up the Number method
-*/
-
-
-/*
-Task 4 - Mood Checker
-
-Do the following:   
-   1. Write a script that prompts the user for their current mood. 
-   2. If the user inputs happy, print 'Yay me too!' to the console, sad print 'Aw cheer up',
-   3. Else just print 'So moody!'
-
-*/
-
-
-/*
-
-
-Task 5 - Odd or Even
-
-Use conditionals to check if a hardcoded number is odd or even, and then console.log the number is odd or even with the numbers value.
-
-*/
-
-var num; // write a number here
-
-// write your conditions here
-
-
-/*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 FIZZBUZZ 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
-
-/*
-
-You're about to do an assignment called "Fizz Buzz", which is one of the classic programming challenges. 
-It is a favorite for interviewers, and a shocking number of job-applicants can't get it right. 
-But you won't be one of those people. Here are the rules for the assignment:
-
-Write a program that prints the numbers from 1 to 100.
-
-But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz".
-
-For numbers which are multiples of both three and five print "FizzBuzz".
-
-The one hint you'll likely need is:
-
-There is a Javascript operator called "remainder" but often referred to as "modulus" or "modulo". 
-It is represented by the percentage sign (%) and it gives you the remainder left over after division. So for example:
-
- 6 % 3
-
-Equals zero. Because after dividing 6 by 3, there is zero leftover. Whereas:
-
-6 % 4
-
-Equals 2. Because after dividing 6 by 4, there are 2 left over from the six.
-
-If that was confusing, don't worry. It will make more sense as you use it. 
-The point is: the remainder operator is useful for finding out if X is a multiple of Y. If it is, then X % Y will yield zero. 
-Knowing this should help you complete this assignment without any issue.
-
-
-Extra Credit:
-
-Instead of only printing "fizz", "buzz", and "fizzbuzz", add a fourth print statement: "prime". 
-You should print this whenever you encounter a number that is prime (divisible only by itself and one).
-As you implement this, don't worry about the efficiency of the algorithm you use to check for primes. 
-It's okay for it to be slow.
-
-
-*/
-
-
-/*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
-
-//Vowel Counter - How many vowels are there?
-/*
-Using the vowelCounter function below do the following:
-  1. Receive a string as a parameter
-  2. Count and return the number of vowels within that string.  It should handle both capitalized and uncapitalized vowels.
-
-  HINT - you may need to study next week's content on arrays 
-  HINT - try looking up the .includes() method
-*/
-
-
-function vowelCounter(/*add your code here*/) {
-    /*add your code here*/
+funcation getcount(str){
+	let vowelscount=0;
+	const vowels = ['a', 'e', 'e' ,'o', 'u'];
+	const strArr = str.split('');
+	strArr.forEach(el)=>{
+		for(i=0 < vowels.length -1; i++)
+			if(el===vowels[i]){
+				vowelscount++
+			}
+	}
+	
+	return vowelscount;
 }
